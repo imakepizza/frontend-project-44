4c214fc5dbb88531148032f42be13f64bc32360d
imakepizza <anpash1317@gmail.com>
2022-11-21 13:55:04 +0900

    fix brain-gcd.js

--- src/games/brain-gcd-logic.js
+++ src/games/brain-gcd-logic.js
@@ -16,13 +16,11 @@ const brainGcd = () => {
   const description = 'Find the greatest common divisor of given numbers.';
   for (let i = 0; i < 3; i += 1) {
     const result = [];
-    let answerGame = '';
-    let question = '';
     const rangeOfNumbers = 100;
     const firstNumber = Math.round(Math.random() * rangeOfNumbers);
     const secondNumber = Math.round(Math.random() * rangeOfNumbers);
-    question = `${firstNumber} ${secondNumber}`;
-    answerGame = (gcd(firstNumber, secondNumber)).toString();
+    const question = `${firstNumber} ${secondNumber}`;
+    const answerGame = (gcd(firstNumber, secondNumber)).toString();
     result.push(question);
     result.push(answerGame);
     gameData.push(result);
