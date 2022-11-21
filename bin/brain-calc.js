b55c66e2c2f2ff6472278bea6f2adbb57c1bb389
imakepizza <anpash1317@gmail.com>
2022-11-21 12:51:20 +0900

    change paths

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import {brainCalc} from "../src/brain-calc-logic.js"
+import {brainCalc} from "../src/games/brain-calc-logic.js"
 
 brainCalc();
--- bin/brain-primes.js
+++ bin/brain-primes.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import {brainPrimes} from "../src/brain-primes-logic.js"
+import {brainPrimes} from "../src/games/brain-primes-logic.js"
 
 brainPrimes();
