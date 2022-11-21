6b0d530ab437b60364084cdc7a0d8b32e0350957
imakepizza <anpash1317@gmail.com>
2022-11-21 11:46:17 +0900

    refactor

--- src/brain-gcd-logic.js
+++ src/brain-gcd-logic.js
@@ -1,11 +1,6 @@
 import readlineSync from 'readline-sync';
+import {gameLogic} from './index.js'
 
-const askName = () => {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
 const gcd = (num1, num2) => {
   let temp = 0;
   while (num2) {
@@ -16,28 +11,22 @@ const gcd = (num1, num2) => {
   return num1;
 }
 const brainGcd = () => {
-  let answerGame = '';
-  let answerUser = '';
-  const userName = askName();
-  let roundCounter = 0;
-  console.log('Find the greatest common divisor of given numbers.');
-  while (roundCounter < 3) {
-    const firstNumber = Math.round(Math.random() * 100);
-    const secondNumber = Math.round(Math.random() * 100);
-    console.log(`Question: ${firstNumber} ${secondNumber}`);
-    answerUser = readlineSync.question('Your answer: ');
-    answerGame = gcd(firstNumber, secondNumber);
-    if (Number(answerUser) === answerGame) {
-      console.log('Correct!');
-      roundCounter += 1;
-    } else {
-      console.log(
-        `'${answerUser}' is wrong answer ;(. Correct answer was '${answerGame}'
-        ,\nLet's try again, ${userName}!`,
-      );
-      return;
+  const gameData = [];
+  const description = 'Find the greatest common divisor of given numbers.';
+  for(let i = 0; i < 3; i += 1) {
+    const result = [];
+    let answerGame = '';
+    let question = '';
+    const rangeOfNumbers = 100;
+    const firstNumber = Math.round(Math.random() * rangeOfNumbers);
+    const secondNumber = Math.round(Math.random() * rangeOfNumbers);
+    question = `${firstNumber} ${secondNumber}`;
+    answerGame = (gcd(firstNumber, secondNumber)).toString();
+    result.push(question);
+    result.push(answerGame)
+    gameData.push(result);
     }
-  }
-  console.log(`Congratulations, ${userName}`);
+  gameLogic(description, gameData);
+
 };
 export { brainGcd };
