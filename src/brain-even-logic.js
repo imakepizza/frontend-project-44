b68432aa3e86d34904a15393cbc7a451e4fdf32a
imakepizza <anpash1317@gmail.com>
2022-11-21 11:52:21 +0900

    refactor

--- src/brain-even-logic.js
+++ src/brain-even-logic.js
@@ -1,34 +1,19 @@
 import readlineSync from 'readline-sync';
+import {gameLogic} from './index.js'
 
-const askName = () => {
-  console.log('Welcome to the Brain Games!')
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  return name;
-};
 const brainEven = () => {
-  let numberToGuess = 0;
-  let answerGame = '';
-  let answerUser = '';
-  let userName = askName();
-  let roundCounter = 0;
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  while(roundCounter < 3) {
-  	numberToGuess = Math.round(Math.random() * 100);
-    console.log(`Question: ${numberToGuess}`);
-    answerGame = numberToGuess % 2 === 0 ? 'yes' : 'no';
-    answerUser = readlineSync.question('Your answer: ');
-    if (answerUser.toLowerCase() === answerGame) {
-      console.log('Correct');
-      roundCounter += 1;
-    } else {
-      console.log(
-        `'${answerUser}' is wrong answer ;(. Correct answer was '${answerGame}'.`,
-        `\nLet's try again, ${userName}!`,
-    );
-    return
-    }
+  const description ='Answer "yes" if the number is even, otherwise answer "no".';
+  const gameData = [];
+  const rangeOfNumbers = 100;
+  for(let i = 0; i < 3; i += 1) {
+    const result = [];
+  	const numberToGuess = Math.round(Math.random() * rangeOfNumbers);
+    const question = `${numberToGuess}`;
+    const answerGame = numberToGuess % 2 === 0 ? 'yes' : 'no';
+    result.push(question);
+    result.push(answerGame);
+    gameData.push(result);
   }
-  console.log(`Congratulations, ${userName}`);
+  gameLogic(description, gameData);
 };
 export { brainEven };
