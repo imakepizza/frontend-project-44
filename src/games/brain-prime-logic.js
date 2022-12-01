590100907f19483c0eeef38a9d732c1cd76ba358
imakepizza <anpash1317@gmail.com>
2022-12-01 15:06:39 +0900

    fix prime

--- src/games/brain-prime-logic.js
+++ src/games/brain-prime-logic.js
@@ -26,7 +26,7 @@ export default () => {
   const gameData = [];
   const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
   for (let i = 0; i < ROUNDS_COUNT; i += 1) {
-    gameData.push(generateRound);
+    gameData.push(generateRound());
   }
   gameLogic(description, gameData);
 };
