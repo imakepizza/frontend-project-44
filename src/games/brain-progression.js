c435d6a38b36b5ef131d6abe0752366bd1bafb9b
imakepizza <anpash1317@gmail.com>
2023-01-03 01:08:58 +0900

    refactor

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,10 +1,12 @@
 import { runEngine, ROUNDS_COUNT } from '../index.js';
 import getRandomInteger from '../utils.js';
 
-const makeProgression = (step, start, end) => {
+const makeProgression = (step, start, length) => {
   const arr = [];
-  for (let i = start; i < end; i += step) {
-    arr.push(i);
+  let temp = start;
+  for (let i = 0; i < length; i += 1) {
+    arr.push(temp);
+    temp += step;
   }
   return arr;
 };
@@ -12,9 +14,9 @@ const makeProgression = (step, start, end) => {
 const generateRound = () => {
   const step = getRandomInteger(1, 10);
   const start = getRandomInteger(0, 150);
-  const end = start + step * 10;
-  const progression = makeProgression(step, start, end);
-  const spacedNumber = getRandomInteger(0, 9);
+  const length= 10
+  const progression = makeProgression(step, start, length);
+  const spacedNumber = getRandomInteger(0, 10);
   const answer = `${progression[spacedNumber]}`;
   progression[spacedNumber] = '..';
   const question = progression.join(' ');
